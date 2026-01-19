e76e98be04117f42492ae558e8213307930105b4
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2026-01-19 21:15:29 +0100

    MOved test to another file

--- Checkers/kernel.cu
+++ Checkers/kernel.cu
@@ -3,6 +3,7 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include "tests.h"
 
 // czarne na gorze
 // uklad 
@@ -13,23 +14,23 @@
 //		6	2
 //	..	5	1
 //	8	4	0
-struct Board {
-    uint32_t white_pawns;
-    uint32_t white_kings;
-    uint32_t black_pawns;
-    uint32_t black_kings;
-
-	uint32_t occupied_white;
-	uint32_t occupied_black;
-
-	uint32_t occupied_total;
-
-	char white_strength;
-	char black_strength;
-
-	char is_white_move;
-	char is_capture;
-};
+//struct Board {
+//    uint32_t white_pawns;
+//    uint32_t white_kings;
+//    uint32_t black_pawns;
+//    uint32_t black_kings;
+//
+//	uint32_t occupied_white;
+//	uint32_t occupied_black;
+//
+//	uint32_t occupied_total;
+//
+//	char white_strength;
+//	char black_strength;
+//
+//	char is_white_move;
+//	char is_capture;
+//};
 
 
 __host__ __device__ void writeBoardToBuff(char buffer[72], uint32_t board, char c) {
@@ -120,24 +121,7 @@ __host__ __device__ void print_bin(uint32_t n)
 }
 
 
-Board startBoard() {
-	Board b;
-	b.white_pawns = 0x31313131;
-	b.white_kings = 0;
-	b.black_pawns = 0x8C8C8C8C;
-	b.black_kings = 0;
-	b.occupied_white = b.white_pawns | b.white_kings;
-	b.occupied_black = b.black_pawns | b.black_kings;
-	b.occupied_total = b.occupied_white | b.occupied_black;
-	
 
-	b.white_strength = 12;
-	b.black_strength = 12;
-	b.is_white_move = true;
-	b.is_capture = false;
-
-	return b;
-}
 
 Board captureBoard() {
 	
@@ -1095,7 +1079,7 @@ Error:
 int main()
 {	
 
-	Board board = kingBlocking1();
+	Board board = startBoard();
 	printBoard(board);
     
 	calcAllMovesCuda(board);
